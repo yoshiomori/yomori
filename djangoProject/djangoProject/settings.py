036f74e19c185dd3f1c16943a209b82e605f2f22
Yoshio Mori <yoshio.mori@hotmail.com>
2022-07-11 17:22:50 -0300

    Corrigindo projeto Node

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
-    'node_modules/bootstrap/dist'
+    'nodeProject/node_modules/bootstrap/dist'
 ]
 
 # Default primary key field type
