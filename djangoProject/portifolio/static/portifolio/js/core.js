c14a87df52cac0c128a370a80eacb2ee4d08e8a2
Yoshio Mori <yoshio.mori@hotmail.com>
2022-07-15 12:14:28 -0300

    WIP: Página inicial

---
+++ djangoProject/portifolio/static/portifolio/js/core.js
@@ -0,0 +1,7 @@
+var html = document.getElementsByTagName('html').item(0);
+var body = document.getElementsByTagName('body').item(0);
+html.classList.add('h-100');
+body.classList.add('h-100');
+body.classList.add('bg-dark');
+body.classList.add('text-white');
+body.classList.add('text-center');
