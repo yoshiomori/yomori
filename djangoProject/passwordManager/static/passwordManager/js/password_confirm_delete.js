b1dc6ee7b60aed28257d22293d1cb403fc468ddf
Yoshio Mori <yoshio.mori@hotmail.com>
2022-07-30 13:47:45 -0300

    Melhorias de estilo: Tela de detalhe da senha

---
+++ djangoProject/passwordManager/static/passwordManager/js/password_confirm_delete.js
@@ -0,0 +1,9 @@
+var html = document.getElementsByTagName('html').item(0);
+var body = document.getElementsByTagName('body').item(0);
+var passwordManagerMenu = document.getElementById('password-manager-menu');
+
+html.classList.add('h-100');
+
+body.classList.add('h-100');
+
+passwordManagerMenu.classList.add('active');
