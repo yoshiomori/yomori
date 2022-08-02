2963734f8b0ef4008f1d0a33b36e1135f60b7b7c
Yoshio Mori <yoshio.mori@hotmail.com>
2022-08-02 06:53:22 -0300

    Melhoreias de código: comentário

--- djangoProject/passwordManager/views/commons/password_field_view_mixin.py
+++ djangoProject/passwordManager/views/commons/password_field_view_mixin.py
@@ -2,6 +2,8 @@ from django.forms import PasswordInput
 
 
 class PasswordFieldViewMixin:
+    """O Widget password gerado no servidor, permite que o input do HTML seja do tipo password mesmo quando o
+     javascript esteja desabilitado"""
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
         form.fields['password'].widget = PasswordInput(attrs=form.fields['password'].widget.attrs, render_value=True)
