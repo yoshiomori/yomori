a5a19ef1945b29adf97096128294656a0733a8be
Yoshio Mori <yoshio.mori@hotmail.com>
2022-09-08 11:56:33 -0300

    Estatística: View para variáveis qualitativas nominais

--- djangoProject/estatistica/urls.py
+++ djangoProject/estatistica/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from estatistica import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('estatistica/', include('estatistica.urls')),
+    path('grafico/barras/', views.GraficoBarrasView.as_view()),
+    path('grafico/barras/js/', views.GraficoBarrasJSView.as_view(), name='grafico-barras-js'),
 ]
--- djangoProject/estatistica/views.py
+++ djangoProject/estatistica/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from estatistica.models import GraficoBarrasParaVariaveisQualitativasNominais
+
+
+class GraficoBarrasView(generic.TemplateView):
+    template_name = 'estatistica/grafico_barras.html'
+
+
+class GraficoBarrasJSView(generic.TemplateView):
+    template_name = 'estatistica/grafico_barras.js'
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        response['Content-Type'] = 'application/javascript'
+        return response
+
+    def get_context_data(self, **kwargs):
+        manager = getattr(GraficoBarrasParaVariaveisQualitativasNominais, 'objects')
+        queryset = manager.filter(variavel_id=self.request.GET.get('variavel_id'))
+        return super().get_context_data(queryset=queryset, **kwargs)
